fe782ee93bc466044572eda77e819982c2249ee4
mafe <mafe@gmail.com>
2023-04-03 14:53:55 -0300

    deu certo

---
+++ main.py
@@ -0,0 +1,7 @@
+ano = int(input(" "))
+idade = int(input(" "))
+outro = int(input(" "))
+nome = input(" ")
+novaIdade = (outro - ano) + idade
+print(nome + ", no ano de " + str(outro) + " você terá " + str(novaIdade) + " anos")
+
